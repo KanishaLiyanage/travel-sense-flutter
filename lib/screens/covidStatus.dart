e862d4271af234d1c4fcd9807bbb93941b6cb601
KanishaLiyanage <kanishaliyanage97@gmail.com>
2022-09-12 09:07:39 +0530

    ui changes

--- lib/screens/covidStatus.dart
+++ lib/screens/covidStatus.dart
@@ -5,6 +5,8 @@ class CovidStatusScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Color(0xFFDDE8F0),
+    );
   }
 }
