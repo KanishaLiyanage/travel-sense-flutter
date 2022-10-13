246261a52d14bfa3aec573d268b9300c880a9af9
KanishaLiyanage <kanishaliyanage97@gmail.com>
2022-10-13 08:46:13 +0530

    commit

--- lib/screens/covidStatus.dart
+++ lib/screens/covidStatus.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
 class CovidStatusScreen extends StatelessWidget {
-  const CovidStatusScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
